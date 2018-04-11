1ec0cd56b7ebb6660095bd7f892b6b6d8b36b200
abboodd <38141887+abboodd@users.noreply.github.com>
2018-04-11 17:40:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,35 @@
-jsconst Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-
-client.on("ready", () => {
-client.user.setPresence({
-  status: 'dnd',
-  game: { 
-     type: 0,
-     name: ' :) ',
-     details:`, هاي `,
-     url: 'http://twitch.tv/Streammingg',
-     state: `تحت الكلام جمب الصوره.`,
-    application_id: '281376075802476544',
-     assets: {
-        small_image: `431886883216293899.`,
-        small_text: ' الكلام الي تبيه يطلع ع الصور ع ( الصغيره ) يعني يوم احد يضغط عليها ' ,
-        large_image: `ID الصوره الكبيره`,
-        large_text: `الكلام الي تبيه يطلع ع الصور ع ( الكبيره ) يعني يوم احد يضغط عليها.` }
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
-    });
 });
 
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 100000 ;
+    var setGame = [`f!help Servers ${client.guilds.size} `,`f!invite Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
+
+});
+
+
+client.login('token'); 
